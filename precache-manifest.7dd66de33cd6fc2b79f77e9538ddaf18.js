0f63ff4f5e05dd0b8f0fbe1abed9680cc31995a6
Sushant Suman <sushantsuman90@gmail.com>
2022-10-15 20:00:24 +0530

    Updates

---
+++ precache-manifest.7dd66de33cd6fc2b79f77e9538ddaf18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a4fd4f27b8fc2827df7",
+    "url": "/TextUtils.github.io/static/css/main.ab8eb9d0.chunk.css"
+  },
+  {
+    "revision": "6a4fd4f27b8fc2827df7",
+    "url": "/TextUtils.github.io/static/js/main.6a4fd4f2.chunk.js"
+  },
+  {
+    "revision": "e45e0075b0567c8b67a9",
+    "url": "/TextUtils.github.io/static/js/1.e45e0075.chunk.js"
+  },
+  {
+    "revision": "61a34e2c3f98e779d682",
+    "url": "/TextUtils.github.io/static/js/2.61a34e2c.chunk.js"
+  },
+  {
+    "revision": "f661e1e49aa6fd8ee665",
+    "url": "/TextUtils.github.io/static/js/runtime~main.f661e1e4.js"
+  },
+  {
+    "revision": "2ee6ea0c9dc3b2a5ac3f38cf1156f22c",
+    "url": "/TextUtils.github.io/index.html"
+  }
+];
